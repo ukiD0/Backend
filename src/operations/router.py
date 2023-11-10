2e6504a8b91aac3abf9a172977a4400e88ca639b
Valentina Korovkina <valentinaaakkk@yandex.ru>
2023-11-11 02:21:41 +0300

    did try except to errors

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi.exceptions import HTTPException
 from src.database import get_async_session
 from .models import operation
 from .schemas import OperationCreate
@@ -14,9 +14,30 @@ router = APIRouter(
 
 @router.get("/")
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    return result.all()
+#endpoint всегда должен иметь try except,также стоит обрабатывать отдельные ошибки(частный случай)
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        # x = 1/0
+        #лучше писать "шаблон" как минимум из-за suc чтобы на фронте было чтот обрабатывать
+        return {
+            "status": "success",
+            "data": result.all(),
+            "details": None,
+            }
+    # except ZeroDivisionError:
+    #     raise HTTPException(status_code = 500, detail ={
+    #         "status":"error",
+    #         "data": None,
+    #         "details":"Не дели на 0"
+    #     })
+    except Exception:
+        #Передать ошибку разработчикам
+        raise HTTPException(status_code = 500, detail ={
+            "status":"error",
+            "data": None,
+            "details": None
+        })
 
 
 @router.post("/")
